aab40b23a2a32d9e59c03ec406b8813d8af9027f
Naman Sehgal <32195473+namansehgal95@users.noreply.github.com>
2023-02-07 13:22:12 -0800

    Update MUX_2x1.sv

--- always_comb_example/MUX_2x1.sv
+++ always_comb_example/MUX_2x1.sv
@@ -1,1 +1,16 @@
-
+module mux_2x1(
+  input logic[1:0] in, 
+  input logic sel, 
+  output logic out
+);
+  
+  //Instead of filling out the event/sensitivity list (represented in commented code on line 8), you can use an always_comb block (shown on line 9)
+//  always @(sel or in)
+  always_comb
+  begin
+     if(sel == 0)
+        out = in[0];
+     else
+       out = in[1]; 
+  end
+endmodule
